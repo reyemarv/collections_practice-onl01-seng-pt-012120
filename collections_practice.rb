4b309cb0d40f57d33af2a0cfe9f49efa1869c0d2
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 20:53:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(array)
+  array.sort {|int1, int2| int1 <=> int2}
+end
 
+def sort_array_desc(array)
+  array.sort.reverse {|int1, int2| int1 <=> int2}
+end
+
+def sort_array_char_count(array)
+  array.sort {|int1, int2| int1.length <=> int2.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array 
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|int| if int.index? == 3
